5a35086dfe54e5b075fc416cf8a9abba48764c94
haruki <71203445+HarukiK0324@users.noreply.github.com>
2025-10-30 19:55:22 +0900

    added test

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -1,14 +1,26 @@
 #include "Point.hpp"
 #include <iostream>
 
-int         main( void ) {
+// int         main( void ) {
 
+//     Point a(0, 0);
+//     Point b(0, 20);
+//     Point c(20, 0);
+
+//     bsp(a, b, c, Point(0.001f, 0.001f)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
+//     bsp(a, b, c, Point(9.999f, 0.001f)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
+//     bsp(a, b, c, Point(10, 10.001f)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
+//     return 0;
+// }
+
+int main(void)
+{
     Point a(0, 0);
     Point b(0, 20);
     Point c(20, 0);
 
-    bsp(a, b, c, Point(0.001f, 0.001f)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
-    bsp(a, b, c, Point(9.999f, 0.001f)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
-    bsp(a, b, c, Point(10, 10.001f)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
+    bsp(a, b, c, Point(1, 1)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
+    bsp(a, b, c, Point(5, 5)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
+    bsp(a, b, c, Point(12, 12)) ? std::cout << "Inside" << std::endl : std::cout << "Outside" << std::endl;
     return 0;
 }
