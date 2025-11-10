45917553d9c3cd9cfd70d3c13c2e2bc290c4d24b
hkasamat <hkasamat@student.42.fr>
2025-11-10 13:14:59 +0900

    fixed msg

--- CPP04/ex03/MateriaSource.cpp
+++ CPP04/ex03/MateriaSource.cpp
@@ -60,6 +60,6 @@ AMateria* MateriaSource::createMateria(std::string const & type)
             return this->Materias[i]->clone();
         }
     }
-    std::cerr << "MateriaSource inventory full, cannot learn more materias." << std::endl;
+    std::cerr << "Materia type unknown, cannot create Materia." << std::endl;
     return 0;
 }
