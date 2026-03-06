d47a91a4813c65f862ab308e15bd695a3a1769fa
haruki <71203445+HarukiK0324@users.noreply.github.com>
2026-03-06 16:52:08 +0900

    fixed timer

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -15,8 +15,29 @@ int main(int argc, char** argv)
         std::cerr << "Usage: ./PmergeMe [numbers...]" << std::endl;
         return 1;
     }
+    std::vector<int> init_vec;
+    for (int i = 1; i < argc; ++i) {
+        try {
+            std::istringstream ss(argv[i]);
+            std::string leftover;
+            int num;
+            if(!(ss >> num) || (ss >> leftover))
+                throw std::invalid_argument("Error");
+            if(num <= 0)
+                throw std::invalid_argument("Error");
+            init_vec.push_back(num);
+        } catch (const std::exception& e) {
+            std::cerr << "Error" << std::endl;
+            return 1;
+        }
+    }
+    std::cout << "Before:   ";
+    for (std::size_t i = 0; i < init_vec.size(); ++i)
+        std::cout << init_vec[i] << " ";
+    std::cout << std::endl;
+    PmergeMe sorter;
+    double start_vec = get_time_us();
     std::vector<int> vec;
-    std::deque<int> deq;
     for (int i = 1; i < argc; ++i) {
         try {
             std::istringstream ss(argv[i]);
@@ -27,21 +48,30 @@ int main(int argc, char** argv)
             if(num <= 0)
                 throw std::invalid_argument("Error");
             vec.push_back(num);
+        } catch (const std::exception& e) {
+            std::cerr << "Error" << std::endl;
+            return 1;
+        }
+    }
+    std::vector<int> sorted = sorter.sort(vec, vec.size());
+    double end_vec = get_time_us();
+    double start_deq = get_time_us();
+    std::deque<int> deq;
+    for (int i = 1; i < argc; ++i) {
+        try {
+            std::istringstream ss(argv[i]);
+            std::string leftover;
+            int num;
+            if(!(ss >> num) || (ss >> leftover))
+                throw std::invalid_argument("Error");
+            if(num <= 0)
+                throw std::invalid_argument("Error");
             deq.push_back(num);
         } catch (const std::exception& e) {
             std::cerr << "Error" << std::endl;
             return 1;
         }
     }
-    std::cout << "Before:   ";
-    for (std::size_t i = 0; i < vec.size(); ++i)
-        std::cout << vec[i] << " ";
-    std::cout << std::endl;
-    PmergeMe sorter;
-    double start_vec = get_time_us();
-    std::vector<int> sorted = sorter.sort(vec, vec.size());
-    double end_vec = get_time_us();
-    double start_deq = get_time_us();
     std::deque<int> sorted_deq = sorter.sort(deq, deq.size());
     double end_deq = get_time_us();
     std::cout << "After:    ";
